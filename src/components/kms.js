17abf778749e57648363ae279b69875db6c4ae58
Ewan Meldrum <ewan.m@tuta.io>
2018-08-12 18:07:51 +0100

    idk

--- src/components/kms.js
+++ src/components/kms.js
@@ -5,7 +5,8 @@ import React, { Component } from 'react';
 class Kms extends Component {
   render() {
     return (
-      <div className="Kms"> please just kill me </div>
+      <div className="Kms"> kill me</div>
+
     )
   }
 }
